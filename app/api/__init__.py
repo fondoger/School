5fd78a979cf7892f4e599c2a184d09be8f51fe2d
Steven Fang <fondoger@outlook.com>
2018-04-08 22:59:17 +0800

    temperorily remove socket.io

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import users, statuses, other, groups, sales, socketio, messages
+from . import users, statuses, other, groups, sales, messages
