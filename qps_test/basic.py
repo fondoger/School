3e2cf36b7fbf4ba03df5bc6b08fa1a5f0eb3f465
Fondoger <fondoger@outlook.com>
2018-10-16 15:41:23 +0800

    Locust test

--- qps_test/basic.py
+++ qps_test/basic.py
@@ -1,3 +1,5 @@
+# Usage: locust -f basic.py
+
 from locust import HttpLocust, TaskSet, task
 
 def status(l):
@@ -9,13 +11,22 @@ def timeline(l):
     }
     l.client.get("/status?type=timeline", headers=headers)
 
+
+
 class UserTasks(TaskSet):
     tasks = [ timeline, status ]
+
+    @task
+    def update(self):
+        self.client.get("/update?platform=android")
 
 
 class WebsiteUser(HttpLocust):
     host = "http://47.93.240.135/api/v1"
 
     task_set = UserTasks
+
+    min_wait = 5000
+    max_wait = 10000
 
 
