d342b5b8733497d43bef30d71f8d68fdd3703650
root <Fondoger>
2018-10-21 00:01:33 +0800

    Changed only one line

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,9 +1,10 @@
-
+from flask import current_app
 
 def logfuncall(func):
 
     def wrapper(*args, **kwargs):
-        print("> funcalled:", func.__name__ + "()", args, kwargs)
+        if current_app.config['DEBUG']:
+            print("> funcalled:", func.__name__ + "()", args, kwargs)
         return func(*args, **kwargs)
 
     return wrapper
