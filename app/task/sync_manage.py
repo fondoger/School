3260cd477b578e739341ef679437bf8ff906e2f2
fondoger <fondoger@outlook.com>
2019-04-29 11:00:01 +0800

    fix bugs

--- app/task/sync_manage.py
+++ app/task/sync_manage.py
@@ -44,7 +44,7 @@ def resume_next_job(job_id_prefix, current_job_id=None):
 def add_sync_jobs(worker_wrapper, account_info_file, interval):
     from app import scheduler
     with open(account_info_file, "rb") as f:
-        accounts = json.loads(f.read())
+        accounts = json.loads(f.read().decode('utf-8'))
         resumed = False
         for account_info in accounts:
             job_id_prefix = account_info['type']
